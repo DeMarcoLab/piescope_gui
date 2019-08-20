d8200916edd7918e51edd70a3490d9f8d50ba1ea
DavidDierickx <dadie1@student.monash.edu>
2019-08-20 15:34:13 +1000

    Fixed call to basler from piescope

--- piescope_gui/inputoutput/main.py
+++ piescope_gui/inputoutput/main.py
@@ -1,37 +1,5 @@
 import skimage.io as io
-from pypylon import pylon
-from pypylon import genicam
-import time
-
-
-class Basler():
-    def __init__(self):
-        super(Basler, self).__init__()
-        self.camera = pylon.InstantCamera(
-            pylon.TlFactory.GetInstance().CreateFirstDevice())
-        print("Using device ", self.camera.GetDeviceInfo().GetModelName());
-        self.camera.Open()
-        self.camera.MaxNumBuffer = 5
-        self.imageCount = 1
-        self.currentImageIndex = 0
-        self.image = []
-
-    def camera_grab(self):
-        self.camera.StartGrabbingMax(self.imageCount)
-        self.image = []
-
-        while self.camera.IsGrabbing():
-            grabResult = self.camera.RetrieveResult(
-                5000, pylon.TimeoutHandling_ThrowException)
-
-            if grabResult.GrabSucceeded():
-                # print("SizeX: ", grabResult.Width)
-                # print("SizeY: ", grabResult.Height)
-                self.image = grabResult.Array
-            else:
-                print("Error: ", grabResult.Errorcode, grabResult.ErrorDescription)
-            grabResult.Release()
-        return self.image
+from piescope.lm import detector
 
 
 def create_array_list(input_list):
@@ -117,7 +85,7 @@ def update_laser_power_4(self):
 
 
 def get_basler_image(self):
-    basler = Basler()
+    basler = detector.Basler()
     self.string_list = ["Basler_image"]
     self.array_list = basler.camera_grab()
     self.update_display()
